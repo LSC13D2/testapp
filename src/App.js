bb61011cb0c3fc4003ba27d1e86c2fa52f7b0e22
Louis-Simon <pro.lsmb@gmail.com>
2021-04-27 17:20:44 +0200

    Fix videos

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 // import logo from './logo.svg';
 import './App.css';
+import 'video.js/dist/video-js.css';
+
+import VideoPlayer from './components/VideoPlayer';
 
 function App() {
-  return (
+
+	const yassineLiveStream = {
+		autoplay: true,
+		controls: true,
+		sources: [{
+			src: 'https://e087512ad0c32643.mediapackage.eu-west-1.amazonaws.com/out/v1/0482f99c52db43e2a55ddb756b935c43/index.m3u8',
+			type: 'application/x-mpegURL'
+		}]
+	};
+
+	const LSLiveStream = {
+		autoplay: true,
+		controls: true,
+		sources: [{
+			src: 'https://18cd60dea8190528.mediapackage.eu-west-3.amazonaws.com/out/v1/4ddfdceaecc44fa684324e408b8492c1/index.m3u8',
+			type: 'application/x-mpegURL'
+		}]
+	};
+
+	return (
     <div className="App">
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -19,9 +41,9 @@ function App() {
         </a>
       </header> */}
 			<h3>Live Stream Yassine</h3>
-			<video source="https://e087512ad0c32643.mediapackage.eu-west-1.amazonaws.com/out/v1/0482f99c52db43e2a55ddb756b935c43/index.m3u8"></video>
+			<VideoPlayer {...yassineLiveStream} />
 			<h3>Live Stream LS</h3>
-			<video source="https://18cd60dea8190528.mediapackage.eu-west-3.amazonaws.com/out/v1/4ddfdceaecc44fa684324e408b8492c1/index.m3u8"></video>
+			<VideoPlayer {...LSLiveStream} />
     </div>
   );
 }
